a0b1c99bbb0974c13da3ceab41792fc3883cad61
Christopher Seander <christopher@seander.se>
2021-11-15 14:15:28 +0100

    moved titled

--- test_chris/layouts.py
+++ test_chris/layouts.py
@@ -9,13 +9,10 @@ sport_statistics = SportStatistics()
 sport_options_dropdown = [{"label" : sport, "value" : sport} for sport in sport_statistics.sports()]
 
 usa_layout = html.Div([
-    html.H1("USA content")
 ])
 
 sports_layout = html.Div([
     dcc.Store(id="sports-data"),
-    html.H1("Sports statistics", className="pt-3"),
-    
     dbc.Row([
         dbc.Col(
             dbc.Card(
