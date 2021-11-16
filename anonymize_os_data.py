d7f0c77578bfd663f8fc35a936a79f48591c35a1
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-11-16 23:36:24 +0100

    Created anonymised csv

--- anonymize_os_data.py
+++ anonymize_os_data.py
@@ -5,4 +5,4 @@ os_data = pd.read_csv("Data/athlete_events.csv")
 
 os_data["Name"] = os_data["Name"].apply(lambda name:hl.sha256(name.encode()).hexdigest())
 
-os_data.to_csv("Data/athlete_events_anonymized.csv", header=True)
+os_data.to_csv("Data/athlete_events_anonymized.csv", index=False, header=True)
