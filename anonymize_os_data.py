079f33ff8a2a404ddd8d9043b4c0c1d7c88bb148
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-11-16 23:29:04 +0100

    Added code

---
+++ anonymize_os_data.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import hashlib as hl
+
+os_data = pd.read_csv("Data/athlete_events.csv")
+
+os_data["Name"] = os_data["Name"].apply(lambda name:hl.sha256(name.encode()).hexdigest())
+
+os_data.to_csv("Data/athlete_events_anonymized.csv", header=True)
