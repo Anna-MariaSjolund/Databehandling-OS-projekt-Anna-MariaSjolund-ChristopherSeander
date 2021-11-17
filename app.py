356cd49279430388834f66cb1d7ce34bc0464c3d
Anna-Maria Sjölund <anna_maria91@hotmail.com>
2021-11-17 15:12:29 +0100

    Updated tab title

--- app.py
+++ app.py
@@ -4,4 +4,5 @@ import dash_bootstrap_components as dbc
 stylesheets = [dbc.themes.DARKLY]
 app = dash.Dash(__name__, suppress_callback_exceptions=True,  external_stylesheets=stylesheets,
                 meta_tags=[dict(name="viewport", content="width=device-width, initial-scale=1.0")]) #For mobile devices
+app.title = "Olympic Games USA"
 server = app.server
