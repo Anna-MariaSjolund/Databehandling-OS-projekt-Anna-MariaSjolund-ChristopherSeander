6faf8673057f2ec1b8024a8f224366ec4a6eae2f
Christopher Seander <christopher@seander.se>
2021-11-17 17:12:14 +0100

    fixed switch

--- load_data.py
+++ load_data.py
@@ -216,6 +216,7 @@ def import_participants_data() -> pd.DataFrame:
 
 class SportStatistics:
 
+
     def __init__(self) -> None:
         read_data = import_world_data()
         sports = read_data[read_data["Sport"].isin(["Alpine Skiing", "Basketball", "Gymnastics", "Rhythmic Gymnastics"])]
